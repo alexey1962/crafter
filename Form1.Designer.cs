0ffe31ff9a8f0234b448447fc07e374aa23bbd70
Alexey <a.kashutin.dev@gmail.com>
2022-05-27 23:05:24 +0600

    create recipes

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,193 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.cell8 = new System.Windows.Forms.Button();
+            this.cell7 = new System.Windows.Forms.Button();
+            this.cell9 = new System.Windows.Forms.Button();
+            this.cell4 = new System.Windows.Forms.Button();
+            this.cell5 = new System.Windows.Forms.Button();
+            this.cell6 = new System.Windows.Forms.Button();
+            this.cell1 = new System.Windows.Forms.Button();
+            this.cell2 = new System.Windows.Forms.Button();
+            this.cell3 = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.Button();
+            this.itemsWrapper = new System.Windows.Forms.FlowLayoutPanel();
+            this.crafter = new System.Windows.Forms.Panel();
+            this.crafter.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cell8
+            // 
+            this.cell8.BackColor = System.Drawing.Color.Transparent;
+            this.cell8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.cell8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cell8.Location = new System.Drawing.Point(51, 13);
+            this.cell8.Name = "cell8";
+            this.cell8.Size = new System.Drawing.Size(32, 32);
+            this.cell8.TabIndex = 0;
+            this.cell8.UseVisualStyleBackColor = false;
+            // 
+            // cell7
+            // 
+            this.cell7.BackColor = System.Drawing.Color.Transparent;
+            this.cell7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.cell7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cell7.Location = new System.Drawing.Point(13, 13);
+            this.cell7.Name = "cell7";
+            this.cell7.Size = new System.Drawing.Size(32, 32);
+            this.cell7.TabIndex = 0;
+            this.cell7.UseVisualStyleBackColor = false;
+            // 
+            // cell9
+            // 
+            this.cell9.BackColor = System.Drawing.Color.Transparent;
+            this.cell9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.cell9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cell9.Location = new System.Drawing.Point(89, 13);
+            this.cell9.Name = "cell9";
+            this.cell9.Size = new System.Drawing.Size(32, 32);
+            this.cell9.TabIndex = 0;
+            this.cell9.UseVisualStyleBackColor = false;
+            // 
+            // cell4
+            // 
+            this.cell4.BackColor = System.Drawing.Color.Transparent;
+            this.cell4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.cell4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cell4.Location = new System.Drawing.Point(13, 51);
+            this.cell4.Name = "cell4";
+            this.cell4.Size = new System.Drawing.Size(32, 32);
+            this.cell4.TabIndex = 0;
+            this.cell4.UseVisualStyleBackColor = false;
+            // 
+            // cell5
+            // 
+            this.cell5.BackColor = System.Drawing.Color.Transparent;
+            this.cell5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.cell5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cell5.Location = new System.Drawing.Point(51, 51);
+            this.cell5.Name = "cell5";
+            this.cell5.Size = new System.Drawing.Size(32, 32);
+            this.cell5.TabIndex = 0;
+            this.cell5.UseVisualStyleBackColor = false;
+            // 
+            // cell6
+            // 
+            this.cell6.BackColor = System.Drawing.Color.Transparent;
+            this.cell6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.cell6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cell6.Location = new System.Drawing.Point(89, 51);
+            this.cell6.Name = "cell6";
+            this.cell6.Size = new System.Drawing.Size(32, 32);
+            this.cell6.TabIndex = 0;
+            this.cell6.UseVisualStyleBackColor = false;
+            // 
+            // cell1
+            // 
+            this.cell1.BackColor = System.Drawing.Color.Transparent;
+            this.cell1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.cell1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cell1.Location = new System.Drawing.Point(13, 89);
+            this.cell1.Name = "cell1";
+            this.cell1.Size = new System.Drawing.Size(32, 32);
+            this.cell1.TabIndex = 0;
+            this.cell1.UseVisualStyleBackColor = false;
+            // 
+            // cell2
+            // 
+            this.cell2.BackColor = System.Drawing.Color.Transparent;
+            this.cell2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.cell2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cell2.Location = new System.Drawing.Point(51, 89);
+            this.cell2.Name = "cell2";
+            this.cell2.Size = new System.Drawing.Size(32, 32);
+            this.cell2.TabIndex = 0;
+            this.cell2.UseVisualStyleBackColor = false;
+            // 
+            // cell3
+            // 
+            this.cell3.BackColor = System.Drawing.Color.Transparent;
+            this.cell3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.cell3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cell3.Location = new System.Drawing.Point(89, 89);
+            this.cell3.Name = "cell3";
+            this.cell3.Size = new System.Drawing.Size(32, 32);
+            this.cell3.TabIndex = 0;
+            this.cell3.UseVisualStyleBackColor = false;
+            // 
+            // result
+            // 
+            this.result.BackColor = System.Drawing.Color.Transparent;
+            this.result.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.result.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.result.Location = new System.Drawing.Point(165, 35);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(64, 64);
+            this.result.TabIndex = 0;
+            this.result.UseVisualStyleBackColor = false;
+            // 
+            // itemsWrapper
+            // 
+            this.itemsWrapper.AutoScroll = true;
+            this.itemsWrapper.Location = new System.Drawing.Point(15, 155);
+            this.itemsWrapper.Margin = new System.Windows.Forms.Padding(5);
+            this.itemsWrapper.Name = "itemsWrapper";
+            this.itemsWrapper.Padding = new System.Windows.Forms.Padding(10);
+            this.itemsWrapper.Size = new System.Drawing.Size(556, 191);
+            this.itemsWrapper.TabIndex = 1;
+            // 
+            // crafter
+            // 
+            this.crafter.AutoSize = true;
+            this.crafter.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.crafter.Controls.Add(this.cell7);
+            this.crafter.Controls.Add(this.result);
+            this.crafter.Controls.Add(this.cell3);
+            this.crafter.Controls.Add(this.cell2);
+            this.crafter.Controls.Add(this.cell1);
+            this.crafter.Controls.Add(this.cell6);
+            this.crafter.Controls.Add(this.cell5);
+            this.crafter.Controls.Add(this.cell4);
+            this.crafter.Controls.Add(this.cell9);
+            this.crafter.Controls.Add(this.cell8);
+            this.crafter.Location = new System.Drawing.Point(13, 13);
+            this.crafter.Name = "crafter";
+            this.crafter.Padding = new System.Windows.Forms.Padding(10);
+            this.crafter.Size = new System.Drawing.Size(242, 134);
+            this.crafter.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.crafter);
+            this.Controls.Add(this.itemsWrapper);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(10);
+            this.Text = "Crafter 2.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.crafter.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button cell8;
+        private System.Windows.Forms.Button cell7;
+        private System.Windows.Forms.Button cell9;
+        private System.Windows.Forms.Button cell4;
+        private System.Windows.Forms.Button cell5;
+        private System.Windows.Forms.Button cell6;
+        private System.Windows.Forms.Button cell1;
+        private System.Windows.Forms.Button cell2;
+        private System.Windows.Forms.Button cell3;
+        private System.Windows.Forms.Button result;
+        private System.Windows.Forms.FlowLayoutPanel itemsWrapper;
+        private System.Windows.Forms.Panel crafter;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -10,11 +10,79 @@ using System.Windows.Forms;
 
 namespace crafter
 {
+    
     public partial class Form1 : Form
     {
+        private static itemRecipe[] candleRecipe = {
+            new itemRecipe { cell = 1, image = "carrot" },
+            new itemRecipe { cell = 2, image = "candle" }
+        };
+        private static itemRecipe[] brickRecipe = {
+            new itemRecipe { cell = 5, image = "book" },
+            new itemRecipe { cell = 7, image = "lead" }
+        };
+        private CrafterConfig[] crafterConfigs =
+        {
+            new CrafterConfig {title = "candle", recipe = candleRecipe },
+            new CrafterConfig {title = "brick", recipe = brickRecipe },
+        };
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void createItem(string name)
+        {
+            Button cellButton = new Button()
+            {
+                Height = 32,
+                Width = 32,
+                BackgroundImage = Image.FromFile("C:/projects/crafter/crafter/temp/item/" + name + ".png"),
+                BackgroundImageLayout = ImageLayout.Stretch,
+            };
+            cellButton.Click += new EventHandler((object sender, EventArgs e) =>
+            {
+                clearCraftingGrid();
+                itemRecipe[] recipe = crafterConfigs.Where(item => item.title == name).First().recipe;
+                updateCraftingGrid(recipe);
+            });
+            itemsWrapper.Controls.Add(cellButton);
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (CrafterConfig item in crafterConfigs)
+            {
+                createItem(item.title);
+            }
+            
+        }
+
+        private void clearCraftingGrid()
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                crafter.Controls["cell" + i].BackgroundImage = null;
+            }
+            result.BackgroundImage = null;
+        }
+
+        private void updateCraftingGrid(itemRecipe[] config)
+        {
+           foreach (var item in config)
+            {
+                crafter.Controls["cell"+item.cell].BackgroundImage = Image.FromFile("C:/projects/crafter/crafter/temp/item/" + item.image + ".png");
+            }
+        }
+    }
+    public struct CrafterConfig
+    {
+        public string title;
+        public itemRecipe[] recipe;
+    }
+
+    public struct itemRecipe {
+        public int cell;
+        public string image;
     }
 }
